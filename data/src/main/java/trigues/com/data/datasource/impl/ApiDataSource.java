ffc1593757c14b9d0629424ed9575d7a16c6aa46
Marc Baqué <marc.baque@est.jediupc.com>
2017-03-18 11:06:26 +0100

    Setup connection with server. Added library for server testing

--- data/src/main/java/trigues/com/data/datasource/impl/ApiDataSource.java
+++ data/src/main/java/trigues/com/data/datasource/impl/ApiDataSource.java
@@ -1,6 +1,13 @@
 package trigues.com.data.datasource.impl;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import trigues.com.data.datasource.ApiInterface;
+import trigues.com.data.service.ServerService;
 
 /**
  * Created by mbaque on 15/03/2017.
@@ -8,8 +15,24 @@ import trigues.com.data.datasource.ApiInterface;
 
 public class ApiDataSource implements ApiInterface {
 
+    private ServerService server;
+
     public ApiDataSource() {
-        //TODO: Preparar connexió al servidor
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addInterceptor(interceptor)
+                .connectTimeout(5, TimeUnit.MINUTES)
+                .writeTimeout(5, TimeUnit.MINUTES)
+                .readTimeout(5, TimeUnit.MINUTES);
+        OkHttpClient client = builder.build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.4.41.147:3000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        server = retrofit.create(ServerService.class);
 
     }
 }
---
+++ data/src/main/java/trigues/com/data/service/ServerService.java
@@ -0,0 +1,8 @@
+package trigues.com.data.service;
+
+/**
+ * Created by mbaque on 18/03/2017.
+ */
+
+public interface ServerService {
+}
