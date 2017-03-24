99dadbaac80a21e888620287bf4ef15f24332a1b
AlbertVVila <albert.val.vila@est.fib.upc.edu>
2017-03-24 12:26:32 +0100

    update layout

--- app/src/main/java/trigues/com/trueke/view/impl/RegisterFragImpl.java
+++ app/src/main/java/trigues/com/trueke/view/impl/RegisterFragImpl.java
@@ -1,9 +1,12 @@
 package trigues.com.trueke.view.impl;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,14 @@ public class RegisterFragImpl extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar_register);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.back);
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        return view;
     }
 }
