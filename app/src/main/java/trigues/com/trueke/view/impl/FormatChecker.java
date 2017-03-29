69407ac9f7a461586fdbae84a8b7034daaa71400
AlbertVVila <albert.val.vila@est.fib.upc.edu>
2017-03-29 12:42:09 +0200

    update

--- app/src/main/java/trigues/com/trueke/view/impl/FormatChecker.java
+++ app/src/main/java/trigues/com/trueke/view/impl/FormatChecker.java
@@ -19,8 +19,14 @@ public class FormatChecker {
 
         return Patterns.PHONE.matcher(target).matches();
     }
-    public final static boolean isValidUser(String target){
+    public final static boolean isValidUser(String target){ // Nom i Cognom?
         return true;
+    }
+    public final static boolean isValidPassword(String target){
+        return target.length()>=5 && target.length()<=20;
+    }
+    public final static boolean isValidDate(String target){
+        return true; //fem Date o String per a data naixement?
     }
 }
 
