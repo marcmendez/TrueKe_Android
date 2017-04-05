d07926b1da48ee92c84afb35adbb2ffb7a9a4d9b
Marc Baqué <marc.baque@est.jediupc.com>
2017-04-05 19:29:32 +0200

    Bug fix

--- app/src/main/java/trigues/com/trueke/view/fragment/RegisterFragImpl.java
+++ app/src/main/java/trigues/com/trueke/view/fragment/RegisterFragImpl.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -127,12 +126,4 @@ public class RegisterFragImpl extends Fragment implements View.OnClickListener {
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
