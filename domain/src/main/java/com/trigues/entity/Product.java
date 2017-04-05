266933e2990c433d570384415df27de571fc127f
Eduard Maestro <eduardmaestro95@gmail.com>
2017-04-05 16:27:23 +0200

    deleted matchs attribute

--- domain/src/main/java/com/trigues/entity/Product.java
+++ domain/src/main/java/com/trigues/entity/Product.java
@@ -17,7 +17,6 @@ public class Product {
     List<String> desiredCategories;
     int minPrice;
     int maxPrice;
-    List<Integer> matches;
 
 
     public Product(int id, int userId, String title, String description, List<String> images, String productCategory, List<String> desiredCategories, int minPrice, int maxPrice) {
@@ -103,6 +102,4 @@ public class Product {
     public void setMaxPrice(int maxPrice) {
         this.maxPrice = maxPrice;
     }
-
-    public int getNumberMatches() { return matches.size();}
 }
