b0311fe41443787da28cd95a62f179300bace172
Marc Baqué <marc.baque@est.jediupc.com>
2017-06-02 14:22:52 +0200

    Fix matchmaking duplicates

--- app/src/main/java/trigues/com/trueke/utils/MatchmakingCard.java
+++ app/src/main/java/trigues/com/trueke/utils/MatchmakingCard.java
@@ -17,7 +17,6 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
 import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
-import com.squareup.picasso.Picasso;
 import com.trigues.entity.Product;
 
 import trigues.com.trueke.R;
@@ -66,17 +65,11 @@ public class MatchmakingCard {
     @SwipeIn
     private void onSwipedIn(){
         callback.onAccepted();
-
-        //TODO: NOMÉS PER TEST
-        mSwipeView.addView(this);
     }
 
     @SwipeOut
     private void onSwipedOut(){
         callback.onRejected();
-
-        //TODO: NOMÉS PER TEST
-        mSwipeView.addView(this);
     }
 
     @SwipeCancelState
